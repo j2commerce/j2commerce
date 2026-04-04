ac03819856d13e8f65e5647e4824327c1109d144
BrianTeeman <brian@teeman.net>
2026-04-04 15:01:38 +0100

    Change vendor company column from td to th with scope="row"

--- administrator/components/com_j2commerce/tmpl/vendors/default.php
+++ administrator/components/com_j2commerce/tmpl/vendors/default.php
@@ -80,11 +80,11 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
                                 <td class="text-center">
                                     <?php echo HTMLHelper::_('jgrid.published', $item->enabled, $i, 'vendors.', true, 'cb'); ?>
                                 </td>
-                                <td>
+                                <th scope="row">
                                     <a href="<?php echo Route::_('index.php?option=com_j2commerce&task=vendor.edit&id=' . $item->j2commerce_vendor_id); ?>">
                                         <?php echo $this->escape($item->company ?: Text::_('COM_J2COMMERCE_NO_COMPANY')); ?>
                                     </a>
-                                </td>
+                                </th>
                                 <td class="d-none d-md-table-cell">
                                     <?php echo $this->escape($item->city ?? ''); ?>
                                 </td>
