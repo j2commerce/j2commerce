ef5f997e8976d98e04aa5428e87b9b0b01e1e4a1
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2026-05-27 17:55:11 -0400

    Version 6.3.3 Preparation (#1105)
    * Version bump to 6.3.3
    
    * Version bump to 6.3.3
    Fixed path to language files on install

--- administrator/components/com_j2commerce/version.php
+++ administrator/components/com_j2commerce/version.php
@@ -13,6 +13,6 @@
 
 // Version constants for J2Commerce
 \define('J2COMMERCE_PRO', true);
-\define('J2COMMERCE_VERSION', '6.3.2');
+\define('J2COMMERCE_VERSION', '6.3.3');
 \define('J2COMMERCE_EXTRA_VERSION', '');
-\define('J2COMMERCE_DATE', '2026-05-22');
+\define('J2COMMERCE_DATE', '2026-05-27');
