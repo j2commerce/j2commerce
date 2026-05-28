88018869fe7e407cf001ef1ef2daa772d3b57016
Olivier <olivier.buisard@simplifyyourweb.com>
2026-05-27 20:16:00 -0400

    SQL fails because an index is applied to the column

--- administrator/components/com_j2commerce/sql/updates/mysql/6.3.2-2026-05-27.sql
+++ administrator/components/com_j2commerce/sql/updates/mysql/6.3.2-2026-05-27.sql
@@ -1,3 +1,11 @@
 -- Widen paymentprofiles.payment_token for gateway token hrefs that exceed 100 chars (e.g. Worldpay verified-token URLs ~117 chars). See issue #1103.
+UPDATE `#__j2commerce_paymentprofiles`
+    SET `payment_token` = ''
+    WHERE `payment_token` IS NULL;
+
 ALTER TABLE `#__j2commerce_paymentprofiles`
-    MODIFY COLUMN `payment_token` varchar(255) NOT NULL DEFAULT '';
+    DROP INDEX `uq_user_provider_env_token` /** CAN FAIL **/;
+ALTER TABLE `#__j2commerce_paymentprofiles`
+    MODIFY `payment_token` VARCHAR(255) NOT NULL DEFAULT '';
+ALTER TABLE `#__j2commerce_paymentprofiles`
+    ADD UNIQUE KEY `uq_user_provider_env_token` (`user_id`,`provider`,`environment`,`payment_token`) /** CAN FAIL **/;
