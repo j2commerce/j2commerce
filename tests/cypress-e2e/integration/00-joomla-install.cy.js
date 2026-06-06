dc09ff7f3a5e521c9588e96208d9377b4de2bb61
Eoin Oliver <git@github.com>
2026-06-06 23:32:03 +0100

    cypress

--- tests/cypress-e2e/integration/00-joomla-install.cy.js
+++ tests/cypress-e2e/integration/00-joomla-install.cy.js
@@ -1,6 +1,6 @@
 describe('Joomla installation', () => {
   it('installs Joomla via web installer', () => {
-    cy.installJoomla();
+    cy.installJoomla(Cypress.env());
     cy.doAdministratorLogin(Cypress.env('username'), Cypress.env('password'));
   });
 });
