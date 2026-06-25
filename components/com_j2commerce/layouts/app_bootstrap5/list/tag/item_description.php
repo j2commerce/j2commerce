0649b4e446a490b8671e1bc4da41c71d99a17d08
Olivier <olivier.buisard@simplifyyourweb.com>
2026-06-25 18:08:28 +0200

    Support short and long descriptions for product descriptions in layouts

--- components/com_j2commerce/layouts/app_bootstrap5/list/tag/item_description.php
+++ components/com_j2commerce/layouts/app_bootstrap5/list/tag/item_description.php
@@ -13,20 +13,33 @@ defined('_JEXEC') or die;
 
 extract($displayData);
 
-if (!$showDescription) {
-    return;
-}
-
-$shortDesc = $product->short_description ?? '';
-if (empty($shortDesc)) {
-    return;
-}
-
 $maxLength = (int) $params->get('list_description_length', 150);
-if ($maxLength > 0 && strlen($shortDesc) > $maxLength) {
-    $shortDesc = substr($shortDesc, 0, $maxLength) . '...';
+
+$shortDesc = '';
+if ($showDescription) {
+    $shortDesc = $product->product_short_desc ?? '';
+    if ($maxLength > 0 && strlen($shortDesc) > $maxLength) {
+        $shortDesc = substr($shortDesc, 0, $maxLength) . '...';
+    }
+}
+
+$longDesc = '';
+if ($showLongDescription) {
+    $longDesc = $product->product_long_desc ?? '';
+    if ($maxLength > 0 && strlen($longDesc) > $maxLength) {
+        $longDesc = substr($longDesc, 0, $maxLength) . '...';
+    }
+}
+
+if (empty($shortDesc) && empty($longDesc)) {
+    return;
 }
 ?>
 <div class="j2commerce-product-description">
-    <?php echo $shortDesc; ?>
+    <?php if (!empty($shortDesc)) : ?>
+        <div class="j2commerce-product-short-desc"><?php echo $shortDesc; ?></div>
+    <?php endif; ?>
+    <?php if (!empty($longDesc)) : ?>
+        <div class="j2commerce-product-long-desc"><?php echo $longDesc; ?></div>
+    <?php endif; ?>
 </div>
