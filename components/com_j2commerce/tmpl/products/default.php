6a8654f5948b55366b9b0ecfd48d68f775ba93e1
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2026-07-17 09:28:42 +0100

    Fix: avoid Bootstrap dependencies in product pages where filters are used (#1312)
    * send_default_email_template needs to default to 0 like config.xml
    
    * Added data-j2commerce-content="products" to better target filters
    
    * Do not rely on col-md- as selectors, it will be different for uikit.
    Used .j2commerce-products-content instead
    Fixed accordions, j2commerceFilterOffcanvas for UiKit
    
    * Added data-j2commerce-content="products" to better target filters

--- components/com_j2commerce/tmpl/products/default.php
+++ components/com_j2commerce/tmpl/products/default.php
@@ -84,7 +84,7 @@ if ($this->params->get('list_show_filter', 1) && $filterPosition === 'left'){
         <?php endif; ?>
 
         <div class="<?php echo $this->params->get('list_show_filter', 1) ? 'col-md-9' : 'col-12'; ?>">
-            <div class="j2commerce-products-content <?php echo $paddingClass;?>">
+            <div class="j2commerce-products-content <?php echo $paddingClass;?>" data-j2commerce-content="products">
                 <?php if ($this->params->get('list_show_top_filter', 1)) : ?>
                     <?php echo $this->loadTemplate('sortfilter'); ?>
                 <?php endif; ?>
