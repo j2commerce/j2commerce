1bf94c7a72df669fcdca799393b651fe2ceee1d3
Olivier <olivier.buisard@simplifyyourweb.com>
2026-09-08 13:40:22 -0400

    feat(j2commerce): show from-price hint in uikit tag listing flexivariable options

--- plugins/j2commerce/app_uikit/layouts/list/tag/item_flexivariableoptions.php
+++ plugins/j2commerce/app_uikit/layouts/list/tag/item_flexivariableoptions.php
@@ -49,6 +49,9 @@ $esc = static fn(string $value): string => htmlspecialchars($value, ENT_QUOTES, 
                     <?php foreach ($option['optionvalue'] as $ov) : ?>
                         <option value="<?php echo $ov['product_optionvalue_id']; ?>"<?php echo ($defaultOptionValueId == $ov['product_optionvalue_id']) ? ' selected' : ''; ?>>
                             <?php echo $esc(Text::_($ov['optionvalue_name'])); ?>
+                            <?php if (($ov['price_from'] ?? null) !== null && $params->get('product_option_price', 1)) : ?>
+                                (<?php echo $esc(Text::_('COM_J2COMMERCE_FROM_PRICE')); ?> <?php echo J2CommerceHelper::product()->displayPrice($ov['price_from'], $product, $params, 'products.view.option'); ?>)
+                            <?php endif; ?>
                         </option>
                     <?php endforeach; ?>
                 </select>
@@ -80,8 +83,13 @@ $esc = static fn(string $value): string => htmlspecialchars($value, ENT_QUOTES, 
                                 <img class="optionvalue-image uk-margin-small-right"
                                      src="<?php echo Uri::root(true) . '/' . $esc($ov['optionvalue_image']); ?>"
                                      alt="<?php echo $esc(Text::_($ov['optionvalue_name'])); ?>" />
+                                <?php echo $esc(Text::_($ov['optionvalue_name'])); ?>
+                            <?php else : ?>
+                                <?php $ovPriceSuffix = (($ov['price_from'] ?? null) !== null && $params->get('product_option_price', 1))
+                                    ? ' (' . Text::_('COM_J2COMMERCE_FROM_PRICE') . ' ' . strip_tags(J2CommerceHelper::product()->displayPrice($ov['price_from'], $product, $params, 'products.view.option')) . ')'
+                                    : ''; ?>
+                                <?php echo $esc(Text::_($ov['optionvalue_name']) . $ovPriceSuffix); ?>
                             <?php endif; ?>
-                            <?php echo $esc(Text::_($ov['optionvalue_name'])); ?>
                         </label>
                     </div>
                 <?php endforeach; ?>
